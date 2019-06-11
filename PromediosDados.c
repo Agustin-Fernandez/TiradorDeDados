5062c9c7ef4f5248793078a0c8bae561afc7e28b
agustin-fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-06-11 14:53:44 -0300

    funcion promedio agregada

--- PromediosDados.c
+++ PromediosDados.c
@@ -3,3 +3,13 @@
 int tirada (int tipo) {
     return (rand() % tipo + 1);
 }
+
+float promedio (int tipo, int N) {
+    int i = 0, suma = 0;
+
+    for(i=0; i<N; i++) {
+        suma += tirada(tipo);
+    }
+
+    return ((float)suma / N);
+}
--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main()
     srand(time(NULL));
 
     /// Entrada
-    printf("%d", tirada(6));
+    printf("%f", promedio(6, 2));
 
     /// Salida
 
