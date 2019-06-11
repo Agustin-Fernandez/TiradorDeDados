ab1f6178da1a9f4cb99eccffe9c53cc7968ed234
agustin-fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-06-11 15:18:10 -0300

    agregada funcion resultado y modificada promedio

--- PromediosDados.c
+++ PromediosDados.c
@@ -4,11 +4,22 @@ int tirada (int tipo) {
     return (rand() % tipo + 1);
 }
 
-float promedio (int tipo, int N) {
+int resultado(int tipo, int cantidad) {
+    int i = 0;
+    int suma = 0;
+
+    for (i = 0; i<cantidad; i++){
+        suma += tirada(tipo);
+    }
+
+    return suma;
+}
+
+float promedio (int tipo, int cant, int N) {
     int i = 0, suma = 0;
 
     for(i=0; i<N; i++) {
-        suma += tirada(tipo);
+        suma += resultado(tipo, cant);
     }
 
     return ((float)suma / N);
--- main.c
+++ main.c
@@ -6,16 +6,18 @@
 int main()
 {
     srand(time(NULL));
-    int tipo, intentos;
+    int tipo, intentos, cantidad;
 
     /// Entrada
     printf("Ingrese el tipo del dado ");
     scanf("%d",&tipo);
-    printf("\nIngrese la cantidad de dados ");
+    printf("Ingrese la cantidad de dados por tirada ");
+    scanf("%d",&cantidad);
+    printf("\nIngrese la cantidad de intentos ");
     scanf("%d",&intentos);
 
     /// Salida
-    printf("%f",promedio(tipo,intentos));
+    printf("%f",promedio(tipo,cantidad,intentos));
 
     return 0;
 }
