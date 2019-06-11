e678c8ceb06dfe8cc7e92a4919c52af56c9ff52b
agustin-fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-06-11 15:10:30 -0300

    Cambiada la especificacion

--- PromediosDados.h
+++ PromediosDados.h
@@ -1,6 +1,9 @@
 /// Tira un solo dado
 int tirada (int tipo);
 
+/// Devuelve la suma de tirar cantidadDados dados de tipo caras
+int resultado (int tipo, int cantidadDados);
+
 /// Devuelve promedio del dado tipo
 float promedio (int tipo, int cantidadTiradas);
 
